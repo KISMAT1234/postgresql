da2eea8af8dff2f8343829ef4c91c0246c3fd2f4
KISMAT1234 <kismatbohora123@gmail.com>
2024-09-14 18:58:43 -0700

    setting up db connection

---
+++ db.js
@@ -0,0 +1,11 @@
+import { Sequelize } from "sequelize";
+
+const connection = () => {
+
+    const sequelize = new Sequelize('database', 'username', 'password', {
+        host: 'localhost',
+        dialect: /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+    });
+
+
+}
