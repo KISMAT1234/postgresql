350c1e428cb5c9398d9d5e5074080277932aff1c
KISMAT1234 <kismatbohora123@gmail.com>
2024-09-16 09:04:49 -0700

    database connection success

--- db.js
+++ db.js
@@ -1,11 +1,26 @@
 import { Sequelize } from "sequelize";
+import { createUserModel } from "./postgres/model/userSchema.js";
 
-const connection = () => {
+const connection = async() => {
 
     const sequelize = new Sequelize('database', 'username', 'password', {
         host: 'localhost',
-        dialect: /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+        dialect:"postgres"
     });
 
+    let UserModel = null
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+        UserModel = await createUserModel(sequelize)
+        await sequelize.sync()
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
 
+}
+
+export default {
+  connection,
+  UserModel
 }
