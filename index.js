fc1086f82b6bc1af704a4dee782517158346ae2d
KISMAT1234 <kismatbohora123@gmail.com>
2024-09-16 09:13:09 -0700

    router middleware added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express'
+import router from './view/route.js'
 
 const app = express()
+app.use(router)
 app.listent(8000, () => {
     console.log('server connected at port 8000 ')
 })
