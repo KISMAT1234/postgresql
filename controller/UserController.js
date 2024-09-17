4cb751ae7d38903f389223d8cfd7b3bf35513b15
KISMAT1234 <kismatbohora123@gmail.com>
2024-09-17 06:45:48 -0700

    user created

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,6 +1,8 @@
+import UserModel  from "../db.js";
+
 export const getAllEmp = async(req,res) =>{
     try{
-       const user =await createUserModel.findAll();
+       const user =await UserModel.findAll();
        console.log(user,'user-data')
        if(user.length==0){
           return res.status(200).json({"error":"users not found"})
@@ -10,4 +12,22 @@ export const getAllEmp = async(req,res) =>{
     catch(error){
        console.log(error)
     }
+}
+
+export const addEmp = async(req,res)=> {
+   const {data} = req.body
+   try{
+      const user =await UserModel.findOne({
+         where:{empId:empId}
+      })
+
+      if(user == null){
+         await UserModel.create(req.body)
+      }
+      console.log(user,'user-data')
+      return res.status(200).json({message:"employee added successfully"})
+   }
+   catch(error){
+      console.log(error)
+   }
 }
