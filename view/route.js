f18d3c1d2182a2a18c4f531d7e66b50def2c67a4
KISMAT1234 <kismatbohora123@gmail.com>
2024-09-17 06:46:16 -0700

    post method added

--- view/route.js
+++ view/route.js
@@ -1,9 +1,9 @@
 import express from "express";
+import { getAllEmp } from "../controller/UserController";
 
 const router = express.Router()
 
-router.get("/getAll", ()=> {
-    console.log("GET")
-})
+router.get("/getAll",getAllEmp)
+router.post("/addEmp", addEmp)
 
 export default router
