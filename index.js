1c1cc3c7bca60f8b5672d146322a8965831103b1
KISMAT1234 <kismatbohora123@gmail.com>
2024-09-17 06:45:57 -0700

    cors configured

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from 'express'
 import router from './view/route.js'
+import cors from 'cors'
 
 const app = express()
+
+app.use(express.json())
+app.use(cors)
 app.use(router)
 app.listent(8000, () => {
     console.log('server connected at port 8000 ')
