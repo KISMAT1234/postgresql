668fb86e9c569debfc02b13e0d9ed833c51abcc9
KISMAT1234 <kismatbohora123@gmail.com>
2024-09-18 07:54:21 -0700

    connection with database

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import router from './view/route.js'
 import cors from 'cors'
+import Connection from './db.js'
 
 const app = express()
-
+Connection()
 app.use(express.json())
 app.use(cors)
 app.use(router)
