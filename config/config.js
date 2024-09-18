e8737a3050c799963ca69b30153c7efbe223b7e2
KISMAT1234 <kismatbohora123@gmail.com>
2024-09-18 07:54:12 -0700

    env data integrated

--- config/config.js
+++ config/config.js
@@ -2,11 +2,11 @@ import dotenv from "dotenv"
 
 export default {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.HOST,
+    "dialect": "postgres"
   },
   "test": {
     "username": "root",
