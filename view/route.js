337ebb6e1fb8e022b31ba4e1a9b9b7775b822e8e
KISMAT1234 <kismatbohora123@gmail.com>
2024-09-20 09:50:34 -0700

    simple change

--- view/route.js
+++ view/route.js
@@ -4,6 +4,6 @@ import { getAllEmp } from "../controller/UserController.js";
 const router = express.Router()
 
 router.get("/getAll",getAllEmp)
-router.post("/addEmp", addEmp)
+// router.post("/addEmp", addEmp)
 
 export default router
