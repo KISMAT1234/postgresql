4b5aa716784818179050958ad72f43547e0fbcb8
KISMAT1234 <kismatbohora123@gmail.com>
2024-09-20 09:49:16 -0700

    configuration change

--- db.js
+++ db.js
@@ -1,25 +1,21 @@
 import { Sequelize } from "sequelize";
 import { createUserModel } from "./postgres/model/userSchema.js";
+import config from "./config/config.js";
 
-const connection = async() => {
-
-    const sequelize = new Sequelize('database', 'username', 'password', {
-        host: 'localhost',
-        dialect:"postgres"
+    const {username,  password, database , host , dialect , port} = config.development
+    const sequelize = new Sequelize(database, username, password, {
+        host:host,
+        dialect:dialect
     });
 
-    let UserModel = null
+    // let UserModel = null
     try {
         await sequelize.authenticate();
         console.log('Connection has been established successfully.');
-        UserModel = await createUserModel(sequelize)
+        // UserModel = await createUserModel(sequelize)
         await sequelize.sync()
       } catch (error) {
         console.error('Unable to connect to the database:', error);
       }
-}
 
-export default  {
-  connection,
-  UserModel
-}
+export default  sequelize
