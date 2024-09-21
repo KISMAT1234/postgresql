b6bf4b4824b2dcaeff765f3e131833eec7ecf27d
KISMAT1234 <kismatbohora123@gmail.com>
2024-09-21 09:54:16 -0700

    products name change

--- db/migrations/20240920141438-create-product.cjs
+++ db/migrations/20240920141438-create-product.cjs
@@ -32,6 +32,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Products');
+    await queryInterface.dropTable('Product');
   }
 };
