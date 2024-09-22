6a7f134b05992cb77c147c3bbb7b5be5658a5904
KISMAT1234 <kismatbohora123@gmail.com>
2024-09-21 20:05:46 -0700

    reconfigured

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-import express from 'express'
-import router from './view/route.js'
-import cors from 'cors'
-import sequelize from './db.js'
+import express from 'express';
+import router from './view/route.js';
+import cors from 'cors';
+import sequelize from './db.js';
 
-const app = express()
+const app = express();
+
+// Middleware setup
+app.use(express.json());
+app.use(cors());  // Note: Added () to call the cors function
+
+// Routes
+app.use(router);
+
 sequelize.authenticate().then(() => {
     app.listen(8000, () => {
-        console.log('server connected at port 8000 ')
-    })
+        console.log('Server connected at port 8000');
+    });
 }).catch((err) => {
-    console.log(err,'error in s')
-})
-app.use(express.json())
-app.use(cors)
-app.use(router)
+    console.log('Error in sequelize authentication:', err);
+});
