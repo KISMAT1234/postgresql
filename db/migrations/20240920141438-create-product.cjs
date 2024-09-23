7eb213166c7d70c1d8a13ec05c5e6bee54a9c6b6
KISMAT1234 <kismatbohora123@gmail.com>
2024-09-23 22:07:29 +0545

    more attribute added

--- db/migrations/20240920141438-create-product.cjs
+++ db/migrations/20240920141438-create-product.cjs
@@ -12,14 +12,31 @@ module.exports = {
       productName: {
         type: Sequelize.STRING
       },
+      isFeatured:{
+        type:Sequelize.BOOLEAN,
+        defaultValue:false,
+        allowNull : false,
+      },
+      productImage:{
+        type:Sequelize.ARRAY(Sequelize.STRING),
+         allowNUll : false
+      },
+      description:{
+        type:Sequelize.TEXT,
+        allowNull:false,
+      },
       price: {
         type: Sequelize.DECIMAL
       },
       size: {
         type: Sequelize.ENUM('xl','2xl','3xl')
       },
-      userId: {
-        type: Sequelize.INTEGER
+      createdBy: {
+        type: Sequelize.INTEGER,
+        refrences: {
+          model:'user',
+          key:'id'
+        }
       },
       createdAt: {
         allowNull: false,
